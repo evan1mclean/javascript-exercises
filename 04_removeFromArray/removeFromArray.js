6274beb4ead184e5d4d1ac3221af218625bdab3d
Evan McLean <evanhughmclean@gmail.com>
2022-04-20 14:18:13 -0400

    Add solution to problem 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...variableArgs) {
+    for (let i = 0; i < variableArgs.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (variableArgs[i] === array[j]) {
+                array.splice(j, 1);
+            }
+        }       
+    }
+    return array;
 };
 
 // Do not edit below this line
