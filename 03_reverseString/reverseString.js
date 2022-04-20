c2fb1607bdddf04aa66d244bc0234d824322f5c0
Evan McLean <evanhughmclean@gmail.com>
2022-04-20 13:16:45 -0400

    Add solution to problem 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitArray = string.split("");
+    let joinArray = [];
+    for (let i = (splitArray.length - 1); i >= 0; i--) {
+        joinArray.push(splitArray[i]);
+    }
+    return string = joinArray.join("");
 };
-
 // Do not edit below this line
 module.exports = reverseString;
