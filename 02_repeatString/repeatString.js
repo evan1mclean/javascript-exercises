6686c896d412f4b4791b2ca28f84c22bafd086bd
Evan McLean <evanhughmclean@gmail.com>
2022-04-20 12:43:02 -0400

    Add solution to problem 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let concatString = "";
+    if (num < 0) {
+        return "ERROR"
+    }
+    else {
+        for (let i = 1; i <= num; i++) {
+            concatString += string;
+        }
+    }
+    return concatString;
 };
 
 // Do not edit below this line
