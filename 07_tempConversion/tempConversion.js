61e990cfa10527b3fcd115641af73a993aba45fe
Evan McLean <evanhughmclean@gmail.com>
2022-04-21 12:01:04 -0400

    Add solution to problem 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let ctemp = (temp - 32) * (5/9);
+  return Number(ctemp.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let ftemp = (temp * (9/5)) + 32;
+  return Number(ftemp.toFixed(1));
 };
 
 // Do not edit below this line
