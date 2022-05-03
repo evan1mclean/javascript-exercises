082976ec9ae7d0ea957831c63a497f2b2042ae2f
Evan McLean <evanhughmclean@gmail.com>
2022-05-03 16:54:36 -0400

    Solution to problem 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    let current = 1
+    let previous = 0
+    let num = 1;
+    if (n < 0) {
+        return "OOPS";
+    }
+    else {
+        for (let i = 1; i < n; i++) {
+            num = current + previous;
+            previous = current;
+            current = num;
+        }
+        return num;
+    }
 };
 
 // Do not edit below this line
