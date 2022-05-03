0b0ae2bbed395fcfd9eccb1aec4dabf91369d59e
Evan McLean <evanhughmclean@gmail.com>
2022-05-03 16:00:06 -0400

    Solution to problem 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[^\w\s\']|_/g, "").replace(/\s/g, '').toLowerCase();
+    let stringReverse = string.split("").reverse().join("")
+    if (stringReverse === string) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
