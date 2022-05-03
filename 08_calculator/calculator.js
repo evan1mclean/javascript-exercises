36bbe6fce8e2e4e2464e2b2f1870c88e95b14519
Evan McLean <evanhughmclean@gmail.com>
2022-05-03 15:02:12 -0400

    Solutions to problem 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  return numbers.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let total = 1;
+  numbers.forEach(item => {
+    total *= item;
+  })
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) {
+    return 1;
+  }
+  else {
+    let total = 1;
+    while (num > 1) {
+      total *= num;
+      num--;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
